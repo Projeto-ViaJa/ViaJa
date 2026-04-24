2cd41925171462a237966d7cdc46c94a6519bbdd
Npaivas <nicholas.silva@sptech.school>
2026-04-24 17:29:55 -0300

    Alteração no docker compose

--- application/src/database/bd-dev.sql
+++ application/src/database/bd-dev.sql
@@ -1,27 +1,25 @@
-DROP DATABASE `viaja_dev`;
-CREATE DATABASE `viaja_dev`;
-USE `viaja_dev`;
+CREATE DATABASE IF NOT EXISTS viaja_dev;
+USE viaja_dev;
 
-CREATE TABLE empresa (
-id_empresa int primary key auto_increment,
-nome_fantasia varchar(100) not null,
-cnpj char(11) not null unique,
-email_empresa varchar(100) not null unique,
-token varchar(45) not null unique
+CREATE TABLE IF NOT EXISTS empresa (
+    id_empresa int primary key auto_increment,
+    nome_fantasia varchar(100) not null,
+    cnpj char(14) not null unique,
+    email_empresa varchar(100) not null unique,
+    token varchar(45) not null unique
 );
 
-CREATE TABLE usuario (
-id_usuario int primary key auto_increment,
-nome varchar (200),
-email_usuario VARCHAR(200) not null,
-senha varchar(10) not null,
-is_admin tinyint,
-fk_empresa int not null,
-constraint fkEmpresaUsuario foreign key (fk_empresa) references empresa(id_empresa) 
+CREATE TABLE IF NOT EXISTS usuario (
+    id_usuario int primary key auto_increment,
+    nome varchar(200),
+    email_usuario varchar(200) not null,
+    senha varchar(10) not null,
+    is_admin tinyint,
+    fk_empresa int not null,
+    constraint fkEmpresaUsuario
+        foreign key (fk_empresa) references empresa(id_empresa)
 );
 
-insert into empresa (nome_fantasia, cnpj, email_empresa, token) values
-('teste1', '12345678911', 'teste@email.com', '123'),
-('teste2', '12345678912', 'teste2@email.com', '456');
-
-select * from empresa;
+INSERT INTO empresa (nome_fantasia, cnpj, email_empresa, token) VALUES
+('teste1','12345678901234','teste@email.com','123'),
+('teste2','12345678901235','teste2@email.com','456');
