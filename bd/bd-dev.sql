e09eb7d179c984c9c6cbf282d309dc9865198eee
Npaivas <nicholas.silva@sptech.school>
2026-04-25 17:22:03 -0300

    Adicionando arquivo Banco de dados para login e cadastro

--- bd/bd-dev.sql
+++ bd/bd-dev.sql
@@ -1,10 +1,25 @@
-CREATE DATABASE `viaja_dev`;
+CREATE DATABASE IF NOT EXISTS viaja_dev;
+USE viaja_dev;
 
-USE `viaja_dev`;
-
-CREATE TABLE usuario (
-email VARCHAR(200) not null,
-token char(64),
-senha varchar(10) not null,
-nome varchar (200)
+CREATE TABLE IF NOT EXISTS empresa (
+    id_empresa int primary key auto_increment,
+    nome_fantasia varchar(100) not null,
+    cnpj char(14) not null unique,
+    email_empresa varchar(100) not null unique,
+    token varchar(45) not null unique
 );
+
+CREATE TABLE IF NOT EXISTS usuario (
+    id_usuario int primary key auto_increment,
+    nome varchar(200),
+    email_usuario varchar(200) not null,
+    senha varchar(10) not null,
+    is_admin tinyint,
+    fk_empresa int not null,
+    constraint fkEmpresaUsuario
+        foreign key (fk_empresa) references empresa(id_empresa)
+);
+
+INSERT INTO empresa (nome_fantasia, cnpj, email_empresa, token) VALUES
+('teste1','12345678901234','teste@email.com','123'),
+('teste2','12345678901235','teste2@email.com','456');
