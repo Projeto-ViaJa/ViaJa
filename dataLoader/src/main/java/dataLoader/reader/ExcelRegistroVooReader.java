7f8d6f75bdf8d2f23bf24a0df8f8a53f8a724649
vicraffa <victor.loliveira@sptech.school>
2026-04-26 16:06:39 -0300

    feature: adicionando o dataLoader versão inicial

---
+++ dataLoader/src/main/java/dataLoader/reader/ExcelRegistroVooReader.java
@@ -0,0 +1,74 @@
+package dataLoader.reader;
+
+import com.github.pjfanning.xlsx.StreamingReader;
+import com.sun.tools.javac.Main;
+import dataLoader.util.RegistroVooColumns;
+import entity.RegistroVoo;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import static dataLoader.util.ExcelUtils.getInt;
+import static dataLoader.util.ExcelUtils.getLong;
+import static dataLoader.util.ExcelUtils.getString;
+
+
+// >>>> explicar classe aqui <<<<
+public class ExcelRegistroVooReader {
+
+    public List<RegistroVoo> extrairRegistros(String nomeArquivo) {
+
+        List<RegistroVoo> registros = new ArrayList<>();
+
+        try (Workbook workbook = new StreamingReader.Builder()
+                .rowCacheSize(100)
+                .bufferSize(4096)
+                .open(new File(nomeArquivo))) {
+
+            Sheet sheet = workbook.getSheetAt(0);
+
+            for (Row row : sheet) {
+                if (row.getRowNum() == 0) continue;
+
+                try {
+                    RegistroVoo r = new RegistroVoo(
+                            getInt(row, RegistroVooColumns.COL_ANO),
+                            getInt(row, RegistroVooColumns.COL_MES),
+                            getString(row, RegistroVooColumns.COL_ORIGEM_LOCALIDADE),
+                            getString(row, RegistroVooColumns.COL_ORIGEM_REGIAO),
+                            getString(row, RegistroVooColumns.COL_ORIGEM_UF),
+                            getString(row, RegistroVooColumns.COL_DESTINO_UF),
+                            getString(row, RegistroVooColumns.COL_DESTINO_REGIAO),
+                            getString(row, RegistroVooColumns.COL_DESTINO_LOCALIDADE),
+                            getString(row, RegistroVooColumns.COL_NATUREZA),
+                            getString(row, RegistroVooColumns.COL_GRUPO_VOO),
+                            getInt(row, RegistroVooColumns.COL_PASSAGEIROS_PAGOS),
+                            getInt(row, RegistroVooColumns.COL_PASSAGEIROS_GRATIS),
+                            getLong(row, RegistroVooColumns.COL_ASK),
+                            getLong(row, RegistroVooColumns.COL_RPK),
+                            getLong(row, RegistroVooColumns.COL_ATK),
+                            getLong(row, RegistroVooColumns.COL_RTK),
+                            getInt(row, RegistroVooColumns.COL_DECOLAGENS),
+                            getInt(row, RegistroVooColumns.COL_ASSENTOS)
+                    );
+
+                    registros.add(r);
+
+                } catch (Exception e) {
+                    System.out.println("Erro na linha " + row.getRowNum());
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return registros;
+    }
+}
