997a364daf501aa6eaff44c8831ba60949eb263d
vicraffa <victor.loliveira@sptech.school>
2026-04-29 00:28:53 -0300

    alter bd-dev.sql

--- bd/bd-dev.sql
+++ bd/bd-dev.sql
@@ -1,6 +1,7 @@
-CREATE DATABASE IF NOT EXISTS viaja_dev;
-USE viaja_dev;
+CREATE DATABASE viaJa;
+USE viaJa;
 
+-- CRIANDO TABELA EMPRESA --
 CREATE TABLE IF NOT EXISTS empresa (
     id_empresa int primary key auto_increment,
     nome_fantasia varchar(100) not null,
@@ -8,7 +9,10 @@ CREATE TABLE IF NOT EXISTS empresa (
     email_empresa varchar(100) not null unique,
     token varchar(45) not null unique
 );
+    
+SELECT * FROM empresa;
 
+-- CRIANDO TABELA USUARIO -- 
 CREATE TABLE IF NOT EXISTS usuario (
     id_usuario int primary key auto_increment,
     nome varchar(200),
@@ -16,10 +20,104 @@ CREATE TABLE IF NOT EXISTS usuario (
     senha varchar(10) not null,
     is_admin tinyint,
     fk_empresa int not null,
+    ativo tinyint default 1,
+    nivel int default 1,
     constraint fkEmpresaUsuario
         foreign key (fk_empresa) references empresa(id_empresa)
 );
 
-INSERT INTO empresa (nome_fantasia, cnpj, email_empresa, token) VALUES
-('teste1','12345678901234','teste@email.com','123'),
-('teste2','12345678901235','teste2@email.com','456');
+SELECT * FROM  usuario;
+
+SELECT e.nome AS "Nome Empresa",
+e.cnpj AS "CNPJ",
+e.emailEmpresa AS "Email Empresa",
+e.tokenEmpresa AS "Token Empresa", 
+u.nome AS "Nome Usuário", 
+u.email AS "Email Usuário",
+u.senha AS "Senha"
+ FROM usuario AS u JOIN empresa AS e
+	ON u.fkEmpresa = e.idEmpresa;
+    
+-- CRIANDO TABELA CALENDÁRIO -- 
+CREATE TABLE eventosRegistrados(
+	idEventosRegistrados INT PRIMARY KEY AUTO_INCREMENT,
+    dataInicial DATE, 
+    horarioInicial TIME, 
+    dataFinal DATE, 
+    horarioFinal TIME,
+    titulo VARCHAR(200),
+    descricao VARCHAR(500), 
+    dataRegistro DATE, 
+    horarioRegistro TIME,
+    fkEmpresa INT, 
+    FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa)
+    );
+    
+SELECT * FROM eventosRegistrados;
+
+SELECT * FROM eventosRegistrados JOIN empresa
+	ON fkEmpresa = idEmpresa;
+    
+-- CRIANDO TABELA BASE 10 ANOS -- 
+CREATE TABLE registro_voo (
+id int primary key auto_increment,
+ano int,
+mes int,
+origem_uf varchar(200),
+origem_regiao varchar(200),
+origem_localidade varchar(200),
+destino_uf varchar(200),
+destino_regiao varchar(200),
+destino_localidade varchar(200),
+natureza varchar(200),
+grupo_voo varchar(200),
+passageiros_pagos int,
+passageiros_gratis int,
+ask long,
+rpk long,
+atk long,
+rtk long,
+decolagens int,
+assentos int
+);
+
+SELECT * FROM base10Anos;
+
+-- CRIANDO TABELA HOSPEDAGENS PARCEIROS --
+CREATE TABLE hospedagemParceiros (
+    idhospedagemParceiros INT PRIMARY KEY AUTO_INCREMENT,
+    cnpj VARCHAR(45),
+    nomeFantasia VARCHAR(500),
+    tipoHospedagem VARCHAR(100),
+    nomeResponsavel VARCHAR(500),
+    telContato VARCHAR(45),
+    email VARCHAR(200),
+    filialOuMatriz VARCHAR(45),
+    uf VARCHAR(45),
+    municipio VARCHAR(45),
+    rua VARCHAR(100),
+    bairro VARCHAR(100),
+    cep VARCHAR(45)
+);
+
+CREATE TABLE hospedagemFavoritos (
+    idhospedagemFavoritos INT PRIMARY KEY AUTO_INCREMENT,
+    fkHospedagem INT,
+    avaliacao VARCHAR(50),
+    comentario VARCHAR(45),
+    dtEdicao VARCHAR(45),
+    fkUsuario INT,
+    fkEmpresa INT,
+    FOREIGN KEY (fkHospedagem) REFERENCES hospedagemParceiros(idhospedagemParceiros),
+    FOREIGN KEY (fkUsuario) REFERENCES Usuario(idUsuario),
+    FOREIGN KEY (fkEmpresa) REFERENCES Empresa(idEmpresa)
+);
+
+CREATE TABLE log (
+id int primary key auto_increment,
+dataEHora datetime default current_timestamp,
+tipo ENUM ('INFO', 'WARNING', 'ERROR'),
+modulo varchar(50),
+mensagem varchar(255),
+descricao text
+);
